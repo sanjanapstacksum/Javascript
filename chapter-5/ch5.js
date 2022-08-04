c7cad4a60c7e0c409ada32e10cde795d1916ff2d
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-04 17:49:28 +0530

    add ch5 code2

--- chapter-5/ch5.js
+++ chapter-5/ch5.js
@@ -47,11 +47,11 @@ colour.name
 
 
 
-const greet={
-    name:"sanjana",
+const greet = {
+    name: "sanjana",
 };
-const you=greet;
-you.name="khushi";
+const you = greet;
+you.name = "khushi";
 greet.name
 //khushi
 
@@ -59,21 +59,260 @@ greet.name
 
 //Objects as Parameters to Functions
 
-function greet({ greeting, name, age }) {
-    return `${greeting}! My name is ${name} and I am ${age} years old.`;
+
+function ok({ greet1ing, name, age }) {
+    return `${greet1ing}! My name is ${name} and I am ${age} years old.`;
 }
 
-greet({ greeting: `What's up dude?`, age: 10, name: `Bart` });
-'What\'s up dude! My name is Bart and I am 10 years old.'
+const final = ok({ greet1ing: `What's up dude?`, age: 22, name: `sanjana` });
+console.log(final)
 
 
-
-function hello({ greeting, name, age }) {
-    return `${greeting}! My name is ${name} and I am ${age} years old.`;
-}
-
-hello({ greeting: `What's up dude?`, age: 22, name: `sanjana` });
-
- 
 // this//
 
+const dice = {
+    sides: 6,
+    roll() {
+
+        return Math.floor(this.sides * Math.random()) + 1;
+    }
+};
+
+//     dice.roll();
+//    // << 5
+//     dice.roll();
+//    // << 3
+
+//     dice.sides = 20;
+//     //<< 20
+
+//     dice.roll();
+//    // << 12
+//     dice.roll();
+//     //<< 18
+
+
+//name spacing//
+
+const myMaths = {
+    square(x) {
+        return x * x;
+    },
+}
+const g = myMaths.square(4);
+console.log(g)
+
+//        o main built-in objects included in JavaScript: arrays and
+// functions. JavaScript has a number of other built-in global objects that can be accessed
+// from anywhere in a program
+
+
+
+//json
+
+const wonderWoman = {
+    name: 'Wonder Woman',
+    'real name': "Diana Prince",
+    height: 182,
+    Weight: 165,
+    hero: true,
+    villain: false,
+    allies: ['Wonder Girl', 'Donna Troy', 'Superman'],
+    lasso: function () {
+        console.log('You will tell the truth!');
+    }
+}
+const json = JSON.stringify(wonderWoman);
+console.log(json)
+
+const ls = wonderWoman.lasso();
+console.log(ls)
+
+//  The stringify() method does the opposite, taking a JavaScript object and returning a
+// string of JSON data, 
+
+// The parse() method takes a string of data in JSON format and returns a JavaScript
+// object:
+
+
+
+// the math object//
+
+// Mathematical Constants
+
+console.log(Math.PI);
+
+console.log(Math.SQRT2);
+
+console.log(Math.SQRT1_2)
+
+console.log(Math.E)
+
+console.log(Math.LN10)
+
+console.log(Math.LOG2E)
+
+console.log(Math.LOG10E)
+
+
+//abs
+
+console.log(Math.abs(3))
+console.log(Math.abs(-4.6));
+// will return positive values
+
+
+// rounding methods
+
+console.log(Math.ceil(4.2))
+
+console.log(Math.ceil(8));
+
+console.log(Math.ceil(-4.2));
+
+console.log(Math.floor(4.2));
+
+console.log(Math.floor(8));
+
+console.log(Math.floor(-4.2));
+
+
+
+//positive value
+console.log(Math.trunc(4.9))
+
+console.log(Math.trunc(-4.2))
+
+
+// The Math.cbrt() method was introduced in ES6, which returns the cube root of numbers:
+console.log(Math.cbrt(8));
+
+console.log(Math.cbrt(-1000));
+
+// the date object
+
+
+const day = new Date();
+console.log(day.toString())
+
+// getter and shatter//
+
+class person{
+    constructor(firstname,lastname,age){
+        this.firstName=firstname;
+        this.lastname=lastname;
+        this.age=age;
+    }
+
+    get fullname(){
+        return `${this.firstName} ${this.lastname}`
+    }
+}
+const person1=new person("sanjana","patel",22)
+console.log(person1.fullname);
+
+
+
+// class person {
+//     constructor(firstname, lastname, age) {
+//         this.firstName = firstname;
+//         this.lastname = lastname;
+//         this.age = age;
+//     }
+
+//     get fullname() {
+//         return `${this.firstName} ${this.lastname}`
+//     }
+
+//     set fullName(fullName) {
+//         const [firstName, lastName] = fullName.split("");
+//         this.firstName=firstName
+//         this.lastName=lastName;
+//     }
+
+
+// }
+
+//  const person1 = new person("sanjana", "patel", 22)
+// console.log(person1.fullname);
+
+// person1.fullName = "khushi patel";
+// console.log(person1)
+
+
+//longest name
+
+// 3 - Write a function which returns the longest country name from provided array of input
+//     e.g. - Input ['India', 'USA', 'UK'] so result here is India
+
+
+function myfunc() {
+    var country = ["india", "US", "UK"]
+    var index = 0;
+    var length = 0;
+    var longestName = "";
+    for (var i = 0; i < country.length; i++) {
+        if (country[i].length > length) {
+            length = country[i].length;
+            longestName = country[i];
+        }
+    }
+
+    console.log(longestName)
+}
+myfunc();
+
+
+// #2 - example array input is [1, 2, 1, 5, 6, 2, 3, 1, 2, 5, 8, 9]
+
+
+
+
+function nonrep(){
+var number =[1, 2, 1, 5, 6, 2, 3, 1, 2, 5, 8, 9]
+var i = 0;
+var j = 0;
+var N = [];
+
+for (i = 0; i < number.length; i++) {
+    for (j = 0; j < number.length; j++) {
+
+        if (i ===j) {
+            continue;
+
+        }
+        if (number[i] === number[j]) {
+            break;
+        }
+        }
+        if(j === number.length)
+        {
+            N.push(number[i]);
+        
+
+    }
+}
+console.log(N);
+}
+
+nonrep();
+
+
+1.// Write a function that clears array from all unnecessary elements, like false, undefined, empty strings, zero, null
+
+const arr = [0, 1, null, 2,3, undefined];
+const f = arr.filter((i) => {
+  return i !== null && typeof i!== 'undefined' && i!==0;
+});
+console.log(f);
+
+
+// [1, 0, null, ' ', false, 5, 6, 7, undefined, null, 0, 9, 10]
+
+
+// #4 - Write a JS function to convert the given number into coins
+//      e.g. 46 - 10, 10, 10, 10, 5, 1
+
+
+
+  
