3574edafdf4604309969eea7ca59c588ece6b61a
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-05 16:06:11 +0530

    add ch6 changes

--- chapter-6/ch6.js
+++ chapter-6/ch6.js
@@ -1,1 +1,26 @@
 console.log("hello world")
+
+//DOM
+
+const body=document.body;
+console.log(typeof body)
+
+const h1=document.getElementById('title');
+console.log(h1)
+
+
+const listItems=document.getElementsByTagName('li');
+console.log(listItems)
+
+const heroes=document.getElementsByClassName('.hero');
+console.log(heroes)
+
+
+
+//query selector//
+
+const query=document.getElementById('ulsection');
+console.log(query);
+
+const add=heroes.appendChild(wonderwomen);
+console.log(add)
