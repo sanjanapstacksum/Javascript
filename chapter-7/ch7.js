926ecb670ab8f9399624a701d378a09707da67d5
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-08 13:26:24 +0530

    add ch7 changes

--- chapter-7/ch7.js
+++ chapter-7/ch7.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+
+
+document.onclick=function(){ console.log('you clicked on the page')}
+
+document.body.addEventListener('click',dosomething);
+addEventListener('click',()=>alert('you Clicked'));
+ 
+function s(){
+    alert('something Happened');
+}
+addEventListener('click',s);
+
+
+function dosomething(event){
+    console.log(event.type);
+}
+addEventListener( 'click',dosomething);
+
+
+
+// const clickParagraph=document.getElementById('click');
+
+// clickParagraph.addEventListener('click',()=> console.log('click'))
+
+// clickParagraph.addEventListener('mousedown',()=> console.log('down' clickParagraph.addEventListener('mouseup',()=>console.log('up'))));
+
