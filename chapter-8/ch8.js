afa1abe749ab990eda14590d41de32ee2c201a1d
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-09 10:02:04 +0530

    add ch8 changes1

--- chapter-8/ch8.js
+++ chapter-8/ch8.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+
+// const form=document.forms[0];
+const form=document.getElementsByTagName('form')[0];
+console.log(form);
+
+const f=document.forms[1]
+console.log(f)
+
+//byname
+// const f=document.forms['myform']
+//const f=document.myform
+// const f=document.getElementbyId(id);
+
+
+// const d=document.form3
+// console.log(d.method)
+
+const d=document.form3
+console.log(d.action)
+
+
+//Form events//
