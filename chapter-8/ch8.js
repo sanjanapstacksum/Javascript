b027a7521f12231cf50f15b12a830a03978330bf
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-10 13:12:40 +0530

    add ch8 changes2

--- chapter-8/ch8.js
+++ chapter-8/ch8.js
@@ -1,38 +1,38 @@
-console.log("hello world");
+// console.log("hello world");
 
-// const form=document.forms[0];
-const form=document.getElementsByTagName('form')[0];
-console.log(form);
+// // const form=document.forms[0];
+// const form=document.getElementsByTagName('form')[0];
+// console.log(form);
 
-const f=document.forms[1]
-console.log(f)
+// const f=document.forms[1]
+// console.log(f)
 
-//byname
-// const f=document.forms['myform']
-//const f=document.myform
-// const f=document.getElementbyId(id);
+// //byname
+// // const f=document.forms['myform']
+// //const f=document.myform
+// // const f=document.getElementbyId(id);
 
+
+// // const d=document.form3
+// // console.log(d.method)
 
 // const d=document.form3
-// console.log(d.method)
-
-const d=document.form3
-console.log(d.action)
+// console.log(d.action)
 
 
-//Form events//
-//focus//
+// //Form events//
+// //focus//
 
-function focusplease(element){
-    element.style.backgroundColor="lime"
-}
+// function focusplease(element){
+//     element.style.backgroundColor="lime"
+// }
 
 
 
-//blur//
-function blurf(element){
-    element.style.backgroundColor=""
-}
+// //blur//
+// function blurf(element){
+//     element.style.backgroundColor=""
+// }
 
 // // document.getElementById("text1").addEventListener("blur",bluron);
 
@@ -55,9 +55,92 @@ function blurf(element){
 
 
 
-//input//
-function inputf(element){
-    const h=element.value
-    document.getElementById("line").innerHTML=h;
+// //input//
+// function inputf(element){
+//     const h=element.value
+//     document.getElementById("line").innerHTML=h;
    
+// }
+// // change event//
+
+// function changed(element){
+//     const h=element.value
+//     document.getElementById("line").innerHTML=h;
+   
+
+//     //select event//
+// }
+// function selected(){
+//     console.log("you selected!!")
+
+// }
+
+
+// //submit event//
+
+// function submited(){
+//     var z=document.getElementById('text1').value;
+//    alert("hello"+ " " +z);
+// }
+
+// document.forms.hero.heroname.focus();
+const form=document.forms['hero'];
+form.addEventListener('submit',validate,false);
+var validated=false;
+function validate(event){
+    const firstLetter=
+    form.heroName.value[0]; 
+    if(firstLetter.toUpperCase()==='X'){
+        event.preventDefault();
+        alert('not allowed')
+        validated=false;
+    }else{
+        validated=true;
+    }
+   
+        
+    }
+
+form.addEventListener('submit',makeHero,false);
+
+
+
+function makeHero(event){
+   
+    const hero={};
+    hero.name=form.heroName.value;
+    hero.Category=form.Category.value;
+    hero.city=form.city.value;
+    hero.origin=form.origin.value
+
+    
+hero.powers=[];
+for(let i=0;i<form.powers.length; i++){
+    if(form.powers[i].checked){
+        hero.powers.push(form.powers[i.value]);
+    }
 }
+
+
+hero.powers=[...form.powers].filter(box=>box.checked).map(box=>box.checked)
+// document.forms.hero.powers[0].checked=true;
+
+    if(validated)
+    alert(JSON.stringify(hero));
+    return hero;
+}
+
+form.powers;
+
+// form.addEventListener('submit',validate,false);
+
+// function validate(event){
+//     const firstLetter=
+//     form.heroName.value[0]; 
+//     if(firstLetter.toUpperCase()==='X'){
+//         event.preventDefault();
+//         alert('not allowed')
+//     }
+
+        
+//     }
