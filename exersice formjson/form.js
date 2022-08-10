f92f9fb19069073c949fcad51f3a4e91ab6f980f
sanjanapstacksum <sanjanap.stacksum@gmail.com>
2022-08-10 17:54:43 +0530

    add ex formjson

--- exersice formjson/form.js
+++ exersice formjson/form.js
@@ -1,1 +1,10 @@
 console.log("hello world")
+
+
+// ----Create a new directory for this in same git repo.
+// 1. Create a HTML Form for user details. Try to cover all inputs (Using bootstrap also)
+// 2. Accept the data on button click and display in table dynamically
+// 3. Add the javascript form validation (For Blank and invalid input)
+// 4. Make form interactive
+
+
